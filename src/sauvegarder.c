f3cf655992f5a38756953287e7f90152861d2937
jeremy/BOURGOUIN <jbourgouin3@gmail.com>
2021-03-25 13:25:04 +0100

    création fichier sauvegarder.c/h

---
+++ src/sauvegarder.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "..\lib\sauvegarder.h"
+
+
+
+void sauvegarder(game_t * game, char * actual_save, SDL_Texture * texture_render_menu_ig){
+
+    SDL_Color blanc = {255,255,255};
+    SDL_Color rouge = {255,0,0};
+
+    SDL_Surface * surf_;
+
+    const Uint8 *keyState = SDL_GetKeyboardState(NULL);
+
+    SDL_bool carac_bool = SDL_TRUE;
+    SDL_Event event;
+
+
+}
