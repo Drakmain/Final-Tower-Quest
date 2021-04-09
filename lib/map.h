05ccfc6e0c4c2330860db52296abb25f6e310720
Slaycers <robinpapillon@gmail.com>
2021-04-09 16:13:01 +0200

    Collision

--- lib/map.h
+++ lib/map.h
@@ -3,45 +3,41 @@
 
 #include "commun.h"
 
-#define NB_IMAGE 1
 
 typedef struct image_s image_t;
 
 struct image_s
 {
-  char *nom_image; /*!< A FINIR. */
-
-  int position_x, position_y; /*!< A FINIR. */
-
-  int largeur, hauteur; /*!< A FINIR. */
-
-  SDL_Rect src, dst; /*!< A FINIR. */
-
-  SDL_Texture *texture; /*!< A FINIR. */
+  char * nom_image;
+  int position_x, position_y;
+  int largeur, hauteur;
+  SDL_Rect src,dst;
+  SDL_Texture* texture;
 };
 
 typedef struct map_s map_t;
 
-struct map_s
+struct map_s 
 {
-  void (*update)(map_t *, SDL_Renderer *, SDL_Rect, SDL_Rect); /*!< Permet la mise a jour d'un objet map_t. */
-  void (*free)(map_t **);                                      /*!< Permet la liberation d'un objet map_t. */
+    void (*update)(map_t *, SDL_Renderer *, SDL_Rect, SDL_Rect); /*!< Permet la mise a jour d'un objet map_t. */
+    void (*free)(map_t **); /*!< Permet la liberation d'un objet map_t. */
 
-  SDL_Texture *texture; /*!< Pointeur sur une texture SDL de l'objet map_t. */
+    SDL_Texture * texture; /*!< Pointeur sur une texture SDL de l'objet map_t. */
 
-  SDL_Surface *surface; /*!< Pointeur sur une surface SDL de l'objet map_t. */
+    SDL_Surface * surface; /*!< Pointeur sur une surface SDL de l'objet map_t. */
 
-  SDL_Rect tile_set; /*!< Tile set de l'objet map_t. */
+    SDL_Rect tile_set; /*!< Tile set de l'objet map_t. */
+    
 
-  //test
-  image_t map_image; /*!< A FINIR. */
+    //test
+    image_t map_image;
 
-  SDL_Surface *map_couleur; /*!< A FINIR. */
+    SDL_Surface* map_couleur;
 
-  int hauteur_carte, largeur_carte; /*!< A FINIR. */
+    int hauteur_carte, largeur_carte;
 };
 
-extern map_t *map_create(SDL_Renderer *render, char *name_map);
-extern SDL_bool map_exist(map_t *const map);
+extern map_t * map_create(SDL_Renderer * render, char * file_name_bmp, char * file_name_txt);
+extern SDL_bool map_exist(map_t * const map);
 
 #endif
